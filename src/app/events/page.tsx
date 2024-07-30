0f347203901cb135404ed32d81c1f5032f95354f
ivanprovci <ivanprovci@yahoo.com>
2024-07-30 15:44:32 -0700

    Events layout + hero image fix
    - events rendered both on home page and events page are fixed
    - hero image is added

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,111 +1,149 @@
-"use client";
-import React, { useState, useEffect } from 'react';
-import ticketmasterService from '../../services/ticketmasterService';
-import SearchForm from './SearchForm';
+"use client"
+import React, { useState, useEffect } from "react"
+import ticketmasterService from "../../services/ticketmasterService"
+import SearchForm from "./SearchForm"
 
 const EventsPage: React.FC = () => {
-  const [events, setEvents] = useState<any[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const eventsPerPage = 10;
-  const [searchParams, setSearchParams] = useState<{ location: string; country: string; category: string }>({
-    location: '',
-    country: '',
-    category: 'All categories'
-  });
+	const [events, setEvents] = useState<any[]>([])
+	const [currentPage, setCurrentPage] = useState(1)
+	const eventsPerPage = 10
+	const [searchParams, setSearchParams] = useState<{
+		location: string
+		country: string
+		category: string
+	}>({
+		location: "",
+		country: "",
+		category: "All categories",
+	})
 
-  useEffect(() => {
-    loadEvents(searchParams.location, searchParams.country, searchParams.category);
-  }, [currentPage, searchParams]);
+	useEffect(() => {
+		loadEvents(
+			searchParams.location,
+			searchParams.country,
+			searchParams.category
+		)
+	}, [currentPage, searchParams])
 
-  const loadEvents = async (location: string, country: string, category: string) => {
-    console.log("Loading events with:", { location, country, category });
-    try {
-      const data = await ticketmasterService.searchEvents({ location, country, category });
-      console.log("Loaded events data:", data);
-      if (data._embedded && data._embedded.events) {
-        setEvents(data._embedded.events);
-      } else {
-        console.warn("No events found");
-        setEvents([]);
-      }
-    } catch (error) {
-      console.error('Error fetching events', error);
-    }
-  };
+	const loadEvents = async (
+		location: string,
+		country: string,
+		category: string
+	) => {
+		console.log("Loading events with:", { location, country, category })
+		try {
+			const data = await ticketmasterService.searchEvents({
+				location,
+				country,
+				category,
+			})
+			console.log("Loaded events data:", data)
+			if (data._embedded && data._embedded.events) {
+				setEvents(data._embedded.events)
+			} else {
+				console.warn("No events found")
+				setEvents([])
+			}
+		} catch (error) {
+			console.error("Error fetching events", error)
+		}
+	}
 
-  const nextPage = () => {
-    setCurrentPage(prevPage => prevPage + 1);
-  };
+	const nextPage = () => {
+		setCurrentPage((prevPage) => prevPage + 1)
+	}
 
-  const prevPage = () => {
-    setCurrentPage(prevPage => prevPage - 1);
-  };
+	const prevPage = () => {
+		setCurrentPage((prevPage) => prevPage - 1)
+	}
 
-  const handleSearch = (location: string, country: string, category: string) => {
-    console.log("Search parameters updated:", { location, country, category });
-    setSearchParams({ location, country, category });
-    setCurrentPage(1);
-  };
+	const handleSearch = (
+		location: string,
+		country: string,
+		category: string
+	) => {
+		console.log("Search parameters updated:", {
+			location,
+			country,
+			category,
+		})
+		setSearchParams({ location, country, category })
+		setCurrentPage(1)
+	}
 
-  const indexOfLastEvent = currentPage * eventsPerPage;
-  const indexOfFirstEvent = indexOfLastEvent - eventsPerPage;
-  const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent);
+	const indexOfLastEvent = currentPage * eventsPerPage
+	const indexOfFirstEvent = indexOfLastEvent - eventsPerPage
+	const currentEvents = events.slice(indexOfFirstEvent, indexOfLastEvent)
 
-  return (
-    <div>
-      <div
-        className="bg-sky-800 w-full flex justify-center items-center text-center p-8 flex-col md:h-450" style={{height: '600px'}}
-      >
-        <h2 className=" text-2xl lg:text-4xl font-bold text-white mt-2">Search for an event you're looking for!</h2>
-        <SearchForm onSearch={handleSearch} />
-      </div>
+	return (
+		<div>
+			<div
+				className="bg-sky-700 w-full flex justify-center items-center text-center p-8 flex-col md:h-450"
+				style={{ height: "600px" }}>
+				<h2 className=" text-2xl lg:text-4xl font-bold text-white mt-2">
+					Search for events:
+				</h2>
+				<SearchForm onSearch={handleSearch} />
+			</div>
 
-      {currentEvents.length > 0 ? (
-        <div className="flex flex-wrap mt-6 p-8 lg:px-24 justify-center items-center space-y-6">
-          {currentEvents.map((event: any) => (
-            <div key={event.id} className="bg-cyan-400  border-4 border-white shadow-lg rounded-lg overflow-hidden w-full md:w-1/2 lg:w-1/3 m-4">
-              <img src={event.images[0].url} alt={event.name} className="w-full h-48 object-cover" />
-              <div className="p-4">
-                <h1 className="font-bold text-2xl mb-2 text-black">{event.name}</h1>
-                <p className="text-gray-600 mb-4">{event.dates.start.localDate}</p>
-                <a href={event.url} target="_blank" rel="noopener noreferrer">
-                  <button
-                    type="button"
-                    className="rounded border-2 border-blue-500 px-4 py-2 text-blue-500 font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-blue-500 hover:text-white"
-                  >
-                    Buy Tickets
-                  </button>
-                </a>
-              </div>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <div className="mt-6 p-8 lg:px-24 text-center">
-          <p>No events found matching your criteria.</p>
-        </div>
-      )}
+			{currentEvents.length > 0 ? (
+				<div className="flex flex-wrap gap-4 mt-6 p-8 justify-center items-stretch">
+					{currentEvents.map((event: any) => (
+						<div
+							key={event.id}
+							className="bg-cyan-400  border-4 border-white shadow-lg rounded-lg overflow-hidden w-full md:w-2/3 lg:w-1/4 flex flex-col">
+							<img
+								src={event.images[0].url}
+								alt={event.name}
+								className="w-full h-48 object-cover"
+							/>
+							<div className="p-4 min-h-max flex flex-col flex-1">
+								<div className="flex-1">
+									<h1 className="font-bold text-2xl mb-2 text-black">
+										{event.name}
+									</h1>
+									<p className="text-gray-600 mb-4">
+										{event.dates.start.localDate}
+									</p>
+								</div>
+								<a
+									href={event.url}
+									target="_blank"
+									rel="noopener noreferrer">
+									<button
+										type="button"
+										className="rounded border-2 border-blue-500 px-4 py-2 text-blue-500 font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-blue-500 hover:text-white">
+										Buy Tickets
+									</button>
+								</a>
+							</div>
+						</div>
+					))}
+				</div>
+			) : (
+				<div className="mt-6 p-8 lg:px-24 text-center">
+					<p>No events found matching your criteria.</p>
+				</div>
+			)}
 
-      {events.length > eventsPerPage && (
-        <div className="flex justify-center items-center mt-4 mb-8">
-          <button
-            onClick={prevPage}
-            disabled={currentPage === 1}
-            className="px-4 py-2 mr-2 bg-blue-500 text-white rounded-lg disabled:opacity-50"
-          >
-            Previous
-          </button>
-          <button
-            onClick={nextPage}
-            disabled={currentEvents.length < eventsPerPage}
-            className="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg disabled:opacity-50"
-          >
-            Next
-          </button>
-        </div>
-      )}
-    </div>
-  );
-};
+			{events.length > eventsPerPage && (
+				<div className="flex justify-center items-center mt-4 mb-8">
+					<button
+						onClick={prevPage}
+						disabled={currentPage === 1}
+						className="px-4 py-2 mr-2 bg-blue-500 text-white rounded-lg disabled:opacity-50">
+						Previous
+					</button>
+					<button
+						onClick={nextPage}
+						disabled={currentEvents.length < eventsPerPage}
+						className="px-4 py-2 ml-2 bg-blue-500 text-white rounded-lg disabled:opacity-50">
+						Next
+					</button>
+				</div>
+			)}
+		</div>
+	)
+}
 
-export default EventsPage;
+export default EventsPage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,98 @@
-"use client";
+"use client"
 
-import React, { useEffect, useState } from 'react';
-import ticketmasterService from '../services/ticketmasterService';
-import partyImage from '../assets/party.png';
-
+import React, { useEffect, useState } from "react"
+import ticketmasterService from "../services/ticketmasterService"
+import partyImage from "../assets/party.png"
 
 export default function Home() {
-  const [popularEvents, setPopularEvents] = useState([]);
+	const [popularEvents, setPopularEvents] = useState([])
 
-  useEffect(() => {
-    const fetchPopularEvents = async () => {
-      try {
-        const data = await ticketmasterService.getPopularEvents();
-        if (data._embedded) {
-          setPopularEvents(data._embedded.events);
-        }
-      } catch (error) {
-        console.error('Error fetching popular events', error);
-      }
-    };
+	useEffect(() => {
+		const fetchPopularEvents = async () => {
+			try {
+				const data = await ticketmasterService.getPopularEvents()
+				if (data._embedded) {
+					setPopularEvents(data._embedded.events)
+				}
+			} catch (error) {
+				console.error("Error fetching popular events", error)
+			}
+		}
 
-    fetchPopularEvents();
-  }, []);
+		fetchPopularEvents()
+	}, [])
 
-  return (
-    <div>
-      {/* Hero section */}
-      <div>
-        <div
-          className=" bg-cover bg-repeat bg-center text-center "
-          style={{
-            backgroundImage: `url(${partyImage})`,
-            height: '400px'
-          }}
-        >
-          <div
-            className="bottom-0 left-0  right-0 top-0 h-full w-full overflow-hidden"
-            style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}
-          >
-            <div className="flex h-full items-center justify-center">
-              <div className="text-white">
-                <h2 className="mb-4 text-4xl font-semibold">Welcome to Ticket Hub</h2>
-                <h4 className="mb-6 text-xl font-semibold">Check out to see all events near you!</h4>
-                <a href="/events">
-                  <button
-                    type="button"
-                    className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                    data-twe-ripple-init
-                    data-twe-ripple-color="light"
-                  >
-                    Find an Event
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <h1 className="text-center text-3xl font-bold mt-8 text-black">Explore Upcoming Events</h1>
-        {/* Event Cards */}
-        <div className="flex flex-wrap mt-6 p-8 lg:px-24 justify-center space-y-6">
-          {popularEvents.length > 0 ? (
-            popularEvents.map((event: any) => (
-              <div
-                key={event.id}
-                className="bg-cyan-400 shadow-lg  border-4 border-white rounded-lg overflow-hidden w-full md:w-1/2 lg:w-1/3 m-4"
-              >
-                <img
-                  src={event.images[0].url}
-                  alt={event.name}
-                  className="w-full h-48 object-cover"
-                />
-                <div className="p-4">
-                  <h1 className="font-bold text-black text-2xl mb-2">{event.name}</h1>
-                  <p className="text-gray-600 mb-4">{event.dates.start.localDate}</p>
-                  <a href={event.url} target="_blank" rel="noopener noreferrer">
-                    <button
-                      type="button"
-                      className="rounded border-2 border-blue-500 px-4 py-2 text-blue-500 font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-blue-500 hover:text-white"
-                    >
-                      Buy Tickets
-                    </button>
-                  </a>
-                </div>
-              </div>
-            ))
-          ) : (
-            <p>Loading events...</p>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			{/* Hero section */}
+			<div>
+				<div className="bg-[url('../assets/party.png')] h-[500px] bg-center">
+					<div
+						className="bottom-0 left-0  right-0 top-0 h-full w-full overflow-hidden"
+						style={{ backgroundColor: "rgba(0, 0, 0, 0.6)" }}>
+						<div className="flex h-full items-center justify-center">
+							<div className="text-white">
+								<h2 className="mb-4 text-4xl font-semibold">
+									Welcome to Ticket Hub
+								</h2>
+								<h4 className="mb-6 text-xl font-semibold">
+									Check out to see all events near you!
+								</h4>
+								<a href="/events">
+									<button
+										type="button"
+										className="rounded border-2 border-neutral-50 px-7 pb-[8px] pt-[10px] text-sm font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
+										data-twe-ripple-init
+										data-twe-ripple-color="light">
+										Find an Event
+									</button>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<h1 className="text-center text-3xl font-bold mt-8 text-black">
+					Explore Upcoming Events
+				</h1>
+				{/* Event Cards */}
+				<div className="flex flex-wrap gap-4 mt-6 p-8 lg:px-24 justify-center items-stretch">
+					{popularEvents.length > 0 ? (
+						popularEvents.map((event: any) => (
+							<div
+								key={event.id}
+								className="bg-cyan-400 shadow-lg  border-4 border-white rounded-lg overflow-hidden w-full md:w-2/3 lg:w-1/4 flex flex-col">
+								<img
+									src={event.images[0].url}
+									alt={event.name}
+									className="w-full h-48 object-cover"
+								/>
+								<div className="p-4 min-h-max flex flex-col flex-1">
+									<div className="flex-1">
+										<h1 className="font-bold text-black text-2xl mb-2">
+											{event.name}
+										</h1>
+										<p className="text-gray-600 mb-4">
+											{event.dates.start.localDate}
+										</p>
+									</div>
+									<a
+										href={event.url}
+										target="_blank"
+										rel="noopener noreferrer">
+										<button
+											type="button"
+											className="rounded border-2 border-blue-500 px-4 py-2 text-blue-500 font-medium uppercase leading-normal transition duration-150 ease-in-out hover:bg-blue-500 hover:text-white">
+											Buy Tickets
+										</button>
+									</a>
+								</div>
+							</div>
+						))
+					) : (
+						<p>Loading events...</p>
+					)}
+				</div>
+			</div>
+		</div>
+	)
 }
